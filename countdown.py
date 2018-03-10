e60bd3e9fa3ddb98a23c5597f24aea332eb8c842
rjlardizabal <rj.lardizabal22@gmail.com>
2018-03-10 10:21:05 +0800

    updated countdown added mousenow

--- countdown.py
+++ countdown.py
@@ -1,10 +1,12 @@
-#!usr/bin/python3
+#python3
 # countdown.py - A simple countdown script.
 
-import time, subprocess  
+import time, subprocess
 
 timeLeft = 60
 while timeLeft > 0:
     print(timeLeft,end='')
     time.sleep(1)
     timeLeft = timeLeft -1
+
+subprocess.Popen(['start','alarm.wav'],shell=True)

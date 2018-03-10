06f020276682e47406be8a9b3cfb4271c88c82a1
rjlardizabal <rj.lardizabal22@gmail.com>
2018-03-10 14:36:44 +0800

    updated mousenow

--- mouseNow.py
+++ mouseNow.py
@@ -7,6 +7,7 @@ try:
     while True:
         x, y = pyautogui.position()
         positionStr = 'X: ' + str(x).rjust(4) + 'Y: ' + str(y).rjust(4)
+        pixelColor = pyautogui.screenshot().getpixel((x, y))
 
 
 except KeyboardInterrupt:
